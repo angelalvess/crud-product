050375dc29bbf991f29848d79d445708c9246010
angelalves <angeltrashangel@gmail.com>
2025-02-10 12:07:06 -0300

    feat: add createProduct and getProductById methods at Controller

--- src/main/java/com/angel/product/controller/ProductController.java
+++ src/main/java/com/angel/product/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.angel.product.controller;
 
 
+import com.angel.product.entity.ProductEntity;
+import com.angel.product.entity.dto.ProductDTO;
 import com.angel.product.entity.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/product")
@@ -14,4 +20,21 @@ public class ProductController {
     private ProductService productService;
 
 
+    @GetMapping("/{productId}")
+    public ResponseEntity<Object> getProductById (@PathVariable("productId") UUID productId) {
+        var product = productService.getProductById(productId);
+
+        if (product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(product.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductEntity> createProduct (@RequestBody ProductDTO productDTO) {
+
+        var product = productService.createProduct(productDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(product);
+    }
 }
--- src/main/java/com/angel/product/entity/services/ProductService.java
+++ src/main/java/com/angel/product/entity/services/ProductService.java
@@ -1,8 +1,14 @@
 package com.angel.product.entity.services;
 
+import com.angel.product.entity.ProductEntity;
+import com.angel.product.entity.dto.ProductDTO;
 import com.angel.product.infra.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ProductService {
@@ -10,6 +16,17 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+
+    public ProductEntity createProduct (ProductDTO productDTO) {
+        var productModel = new ProductEntity();
+        BeanUtils.copyProperties(productDTO, productModel);
+        return productRepository.save(productModel);
+    }
+
+    public Optional<ProductEntity> getProductById (UUID productId) {
+        return productRepository.findById(productId);
+
+    }
 
 
 }
