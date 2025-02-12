7bd68ece46200dd500daebf7cbf5feb01927e522
angelalves <angeltrashangel@gmail.com>
2025-02-12 03:20:58 -0300

    feat: finish project

--- src/main/java/com/angel/product/entity/dto/ProductDTO.java
+++ src/main/java/com/angel/product/entity/dto/ProductDTO.java
@@ -3,5 +3,5 @@ package com.angel.product.entity.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
-public record ProductDTO(@NotBlank String name, @Min(0) Double price) {
+public record ProductDTO(@NotBlank String name, @Min(1) Double price) {
 }
